88e7256b7f195ca747a6696c399f6dc887e6c669
Soumen Gharui <itsmesoumengharui@gmail.com>
2023-05-22 11:10:58 +0530

    8th commmit

--- gesture/views.py
+++ gesture/views.py
@@ -201,12 +201,13 @@ def mouse(request):
 
     import cv2
     import mediapipe as mp
-    import pyautogui
+    import pyautogui # it is for controlling the mouse and keyboard
     import math
     from enum import IntEnum
     from ctypes import cast, POINTER
     from comtypes import CLSCTX_ALL
     from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+    # Pycaw is used for controlling the volume
     from google.protobuf.json_format import MessageToDict
     import screen_brightness_control as sbcontrol
 
